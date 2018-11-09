44b762ede35d45f33c5e869669c7b5aa65d180b6
DESKTOP-CAQ9A79\JYUN3 <jacob.yun429@gmail.com>
2018-11-08 18:50:57 -0800

    connected to MongoDB with mongoose

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://test:password1@ds149495.mlab.com:49495/devconnector'
+}
--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// DB config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB connected')) //if it connects console log
+  .catch(err => console.log(err)); // if it fails
 
 app.get('/', (req, res) => res.send('Hello~'));
 
