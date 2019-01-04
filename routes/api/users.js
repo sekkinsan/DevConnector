1514f56e9273b1b90d8bb9bbd08f98f72fdd968f
DESKTOP-CAQ9A79\JYUN3 <jacob.yun429@gmail.com>
2019-01-03 18:02:42 -0800

    added gravatar for avatar use

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+
+// load user model
+const User = require('../../models/User');
 
 // @route GET api/users/test
 // @desc Tests users route
@@ -7,5 +11,30 @@ const router = express.Router();
 router.get('/test', (req, res) => res.json({
   msg: "Users works properly"
 }));
+
+// @route GET api/users/register
+// @desc register user
+// @access public
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if(user) {
+        return res.status(400).json({email: 'Email already exists in system'});
+      } else {
+        const avatar = gravatar.url(req.body.email, {
+          s: '200', // size
+          r: 'pg', //rating
+          d: 'mm' //default avatar
+        });
+
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          avatar,
+          password: req.body.password
+        });
+      }
+    })
+});
 
 module.exports = router;
