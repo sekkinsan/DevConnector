c7444f51c72deddfddaa35a4eb7b62cd0b4fe5dd
DESKTOP-CAQ9A79\JYUN3 <jacob.yun429@gmail.com>
2019-01-04 19:06:44 -0800

    added validations for email, password, and confirm password

--- validation/register.js
+++ validation/register.js
@@ -5,8 +5,41 @@ const isEmpty = require('./is-empty');
 module.exports = function validateRegisterInput(data) {
   let errors = {};
 
+  data.name = !isEmpty(data.name) ? data.name : '';
+  data.email = !isEmpty(data.email) ? data.email : '';
+  data.password = !isEmpty(data.password) ? data.password : '';
+  data.passwordConfirmed = !isEmpty(data.passwordConfirmed) ? data.passwordConfirmed : '';
+
   if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
     errors.name = 'Name must be between 2 and 30 characters';
+  }
+
+  if (Validator.isEmpty(data.name)) {
+    errors.name = 'Name cannot be blank';
+  }
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = 'Email field is required';
+  }
+
+  if (Validator.isEmail(data.email)) {
+    errors.email = 'Email is invalid';
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.password = 'Password field is required';
+  }
+
+  if (Validator.isLength(data.password, { min: 6, max: 30})) {
+    errors.password = 'Password must at least 6 characters'
+  }
+
+  if (Validator.isEmpty(data.passwordConfirmed)) {
+    errors.passwordConfirmed = 'Confirm Password field is required';
+  }
+
+  if (Validator.equals(data.password, data.passwordConfirmed)) {
+    errors.passwordConfirmed = 'Passwords must match';
   }
 
   return {
