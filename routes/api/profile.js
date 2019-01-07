2451365720d9513f36927e769eb55f42565970a2
DESKTOP-CAQ9A79\JYUN3 <jacob.yun429@gmail.com>
2019-01-06 20:27:44 -0800

    completed current user profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Load profile model
+const Profile = require('../../models/Profile');
+// Load user profile
+const User = require('../../models/User');
 
 // @route GET api/profile/test
 // @desc Tests profile route
@@ -7,5 +14,25 @@ const router = express.Router();
 router.get('/test', (req, res) => res.json({
   msg: "Profile works properly"
 }));
+
+
+// @route GET api/profile
+// @desc Get current user's profile
+// @access private
+router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  // initialize errors
+  const errors = {};
+
+  Profile.findOne({ user: req.user.id })
+    .then(profile => {
+      if(!profile) {
+        errors.noprofile = 'There is no profile for this user';
+        return res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
 
 module.exports = router;
