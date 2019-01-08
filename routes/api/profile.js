351c578293ecfb136659ee00cec5c45bf089c5c0
DESKTOP-CAQ9A79\JYUN3 <jacob.yun429@gmail.com>
2019-01-07 19:44:04 -0800

    completed profile field validations, created profile validation file

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const passport = require("passport");
 
+// Load Validation
+const validateProfileInput = require("../../validation/profile");
+
 // Load profile model
 const Profile = require("../../models/Profile");
 // Load user profile
@@ -28,6 +31,7 @@ router.get(
     const errors = {};
 
     Profile.findOne({ user: req.user.id })
+      .populate("user", ["name", "avatar"])
       .then(profile => {
         if (!profile) {
           errors.noprofile = "There is no profile for this user";
@@ -46,6 +50,14 @@ router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
+    const { errors, isValid } = validateProfileInput(req.body);
+
+    //Check Validation
+    if (!isValid) {
+      // Return any errors with 400 status
+      return res.status(400).json(errors);
+    }
+
     // Get fields which come from the form
     const profileFields = {};
 
