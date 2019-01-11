d8a3ef0a4679c0bffd861334ad5f3c6fbacaf9b8
DESKTOP-CAQ9A79\JYUN3 <jacob.yun429@gmail.com>
2019-01-10 18:47:35 -0800

    Get all posts and get post by ID completed

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -18,6 +18,27 @@ router.get("/test", (req, res) =>
   })
 );
 
+// @route GET api/posts
+// @desc Get all posts
+// @access Public
+router.get("/", (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ nopostsfound: "No posts found" }));
+});
+
+// @route GET api/posts/:id
+// @desc Get post by id
+// @access Public
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err =>
+      res.status(404).json({ nopostfound: "No post found with that ID." })
+    );
+});
+
 // @route POST api/posts
 // @desc Create post
 // @access Private
