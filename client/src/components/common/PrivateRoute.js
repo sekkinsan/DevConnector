59540d6eab8ef266ab62d54dce2ccc4a206d2884
DESKTOP-CAQ9A79\JYUN3 <jacob.yun429@gmail.com>
2019-04-30 20:40:01 -0700

    added privateroute for proper log out redirect so it does not show the dashboard for someone who is not logged in and authenticated section 9.4

---
+++ client/src/components/common/PrivateRoute.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+const PrivateRoute = ({ component: Component, auth, ...rest }) => (
+  //check to see if we are authenticated, if we are it will load the component, if we are not redirect to login
+  <Route
+    {...rest}
+    render={props =>
+      auth.isAuthenticated === true ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to="/login" />
+      )
+    }
+  />
+);
+
+PrivateRoute.propTypes = {
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(PrivateRoute);
